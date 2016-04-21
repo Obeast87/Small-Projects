98b727bb1467fbcd746435406b49d29ae1825d82
David Hendren <obeast@gmail.com>
2016-04-21 01:51:10 -0700

    Update random number.py

--- random number.py
+++ random number.py
@@ -4,10 +4,10 @@ def subtract():
 	x = randint(1, 100)
 	y = randint(1, 100)
 	z = (y - x)
+	print "%s" "-" "%s" " =" " %s"  % (y, x, z)
 	if z < 0:
-		print z
 		subtract()
 	else:
-		print z
-
+		return
+	
 subtract()
